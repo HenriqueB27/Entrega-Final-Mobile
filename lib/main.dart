b5cd89f5cb3fe48e0fcbed9a7c1f7e5bfafdbcb0
Henrique Braz <106036711+HenriqueB27@users.noreply.github.com>
2023-10-22 17:05:13 -0300

    Update main.dart
    Adicionando os métodos de remover e adicionar itens

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,122 @@
-import 'packege:trampo/entregafinal/home.dart';
 import 'package:flutter/material.dart';
 
-
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Supermercado',
+      title: 'Lista de Compras',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      routes: {
-        "/" :(context) => HomeWidget(),
-      },
-      initialRoute: "/",
+      home: ShoppingListScreen(),
+    );
+  }
+}
+
+class ShoppingListScreen extends StatefulWidget {
+  @override
+  _ShoppingListScreenState createState() => _ShoppingListScreenState();
+}
+
+class _ShoppingListScreenState extends State<ShoppingListScreen> {
+  List<String> _shoppingItems = [];
+  Set<String> _checkedItems = {};
+
+  void _addItem(String item) {
+    setState(() {
+      _shoppingItems.add(item);
+    });
+  }
+
+  void _removeCheckedItems() {
+    setState(() {
+      _shoppingItems.removeWhere((item) => _checkedItems.contains(item));
+      _checkedItems.clear();
+    });
+  }
+
+  void _toggleItem(String item) {
+    setState(() {
+      if (_checkedItems.contains(item)) {
+        _checkedItems.remove(item);
+      } else {
+        _checkedItems.add(item);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Lista de Compras'),
+      ),
+      body: ListView(
+        children: _shoppingItems.map((item) {
+          return ListTile(
+            title: Text(item),
+            trailing: Checkbox(
+              value: _checkedItems.contains(item),
+              onChanged: (value) {
+                _toggleItem(item);
+              },
+            ),
+          );
+        }).toList(),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  String newItem = '';
+                  return AlertDialog(
+                    title: const Text('Adicionar Item'),
+                    content: TextField(
+                      onChanged: (value) {
+                        newItem = value;
+                      },
+                    ),
+                    actions: <Widget>[
+                      TextButton(
+                        child: const Text('Cancelar'),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                      TextButton(
+                        child: const Text('Adicionar'),
+                        onPressed: () {
+                          _addItem(newItem);
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+            tooltip: 'Adicionar Item',
+            child: const Icon(Icons.add),
+          ),
+          SizedBox(height: 16),
+          FloatingActionButton(
+            onPressed: () {
+              _removeCheckedItems();
+            },
+            tooltip: 'Remover Itens Marcados',
+            child: const Icon(Icons.delete),
+          ),
+        ],
+      ),
     );
   }
 }
